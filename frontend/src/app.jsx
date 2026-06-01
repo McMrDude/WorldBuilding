f4b9d273127f46f6e1c2645ee4a41e94c899f6cc
Mr penington <benjaminbkris@gmail.com>
2026-06-01 14:03:02 +0200

    e

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -29,15 +29,17 @@ function App() {
             Create a new world
         </button>
 
-        {worlds.map(Worlds => (
-          <div className="world-card" key={Worlds.id} onClick={() => console.log(Worlds.id)}>
-            <h2>World Name:</h2>
-            <h3>{Worlds.worldname}</h3>
+        <div id="worlds-container">
+          {worlds.map(Worlds => (
+            <div className="world-card" key={Worlds.id} onClick={() => console.log(Worlds.id)}>
+              <h2>World Name:</h2>
+              <h3>{Worlds.worldname}</h3>
 
-            <h2>Description:</h2>
-            <h3>{Worlds.description}</h3>
-          </div>
-        ))}
+              <h2>Description:</h2>
+              <h3>{Worlds.description}</h3>
+            </div>
+          ))}
+        </div>
 
         {worldBoxOpen && (
           <>
