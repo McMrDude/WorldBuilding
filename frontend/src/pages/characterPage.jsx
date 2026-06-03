7f1a7781ddc225f476d89d376d013deeb0581a8f
Mr penington <benjaminbkris@gmail.com>
2026-06-03 09:26:15 +0200

    Update characterPage.jsx

--- frontend/src/pages/characterPage.jsx
+++ frontend/src/pages/characterPage.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react';
 import CreateCharacter from '../components/createCharacterBox.jsx';
+import { Link, useParams } from 'react-router-dom'
 
 function CharacterPage() {
     const [characterBoxOpen, setCharacterBoxOpen] = useState(false);
     const [characters, setCharacters] = useState([]);
+    const { id } = useParams();
     
 
     const fetchCharacters = () => {
@@ -20,6 +22,14 @@ function CharacterPage() {
         <div>
             <button onClick={() => setCharacterBoxOpen(true)}>Create Character</button>
 
+            {characters.map(character => (
+                character.world_id === id && (
+                    <div key={character.id}>
+                        <h2>Name: {character.name}</h2>
+                        <p>Description: {character.description}</p>
+                    </div>
+                )
+            ))}
 
             {characterBoxOpen && (
             <>
