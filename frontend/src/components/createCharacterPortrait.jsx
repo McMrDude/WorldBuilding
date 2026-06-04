20138beefc428d85d77f7bce955bdc424b1f24ef
Mr penington <benjaminbkris@gmail.com>
2026-06-04 09:22:59 +0200

    Update createCharacterPortrait.jsx

--- frontend/src/components/createCharacterPortrait.jsx
+++ frontend/src/components/createCharacterPortrait.jsx
@@ -25,9 +25,7 @@ function CreateCharacter({ onClose, onCharacterCreated }) {
             .from("character_portraits")
             .getPublicUrl(fileName);
 
-/*         const publicUrl = publicUrlData.publicUrl;
 
-        console.log(publicUrl); */
 
         if (!imageFile) return;
     }
