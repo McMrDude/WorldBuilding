44e131622398e46ac9906989aa0cd9d16fbd2f74
Mr penington <benjaminbkris@gmail.com>
2026-06-05 10:48:56 +0200

    Update createCharacterBox.jsx

--- frontend/src/components/createCharacterBox.jsx
+++ frontend/src/components/createCharacterBox.jsx
@@ -62,7 +62,7 @@ function CreateCharacter({ onClose, onCharacterCreated }) {
                     <CreateCharacterPortrait
                         name={characterName}
                         description={description}
-                        onClose={() => setImgBoxOpen(false), onCharacterCreated, onClose}
+                        onClose={() => setImgBoxOpen(false), (onCharacterCreated, onClose)}
                     />
                 )}
             </div>
