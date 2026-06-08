4dcdda5871f2f7894d04c4f11aa760c2df2767fc
Mr penington <benjaminbkris@gmail.com>
2026-06-08 10:09:16 +0200

    Update drawBox.jsx

--- frontend/src/components/drawBox.jsx
+++ frontend/src/components/drawBox.jsx
@@ -8,6 +8,8 @@ function Draw({ onClose, onDrawn, onSaveDrawing }) {
     useEffect(() => {
         const canvas = canvasRef.current;
         const ctx = canvas.getContext("2d");
+        ctx.fillStyle = "#ffffff"
+        ctx.fillRect(0, 0, canvas.width, canvas.height)
 
         canvas.width = 400;
         canvas.height = 400;
