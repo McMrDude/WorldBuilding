502b2e83a6514c6a733382345de78832f0a94eee
Mr penington <benjaminbkris@gmail.com>
2026-06-09 13:10:00 +0200

    Update specificCharacterPage.jsx

--- frontend/src/pages/specificCharacterPage.jsx
+++ frontend/src/pages/specificCharacterPage.jsx
@@ -49,7 +49,7 @@ function SpecificCharacter() {
                         { editState ? <p>{character.lore}</p> : <textarea value={lore} ref={loreArea} placeholder='Write your awsome pogchamp sigma lore you dweeb'></textarea>}
                     </div>
                     <div>
-                        { editState ? <button onClick={() => setEditState(false)}>Edit</button> : (<> <button onClick={changeLore}>Save</button> <button onClick={() => setEditState(false)}>Cancel</button> </>)}
+                        { editState ? <button onClick={() => setEditState(false)}>Edit</button> : (<> <button onClick={changeLore}>Save</button> <button onClick={() => setEditState(true)}>Cancel</button> </>)}
                     </div>
                 </div>
             )}
