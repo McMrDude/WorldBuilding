7c10a31914ee941487062b3249f00b3197da818e
Mr penington <benjaminbkris@gmail.com>
2026-06-10 10:46:47 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -110,6 +110,10 @@ app.put("/api/characters/:characterID", async (req, res) => {
 /*SERVER START*/
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
