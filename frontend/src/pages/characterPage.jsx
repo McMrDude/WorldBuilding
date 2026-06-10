8a6c9ea6159f0aca635ed28fa2799366527a7092
Mr penington <benjaminbkris@gmail.com>
2026-06-10 10:27:07 +0200

    Update characterPage.jsx

--- frontend/src/pages/characterPage.jsx
+++ frontend/src/pages/characterPage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import CreateCharacter from '../components/createCharacterBox.jsx';
 import { Link, useParams } from 'react-router-dom'
 import './characterPage.css';
