997cd796451d317dc4dae80f6ae3668573a0ba9e
Mr penington <benjaminbkris@gmail.com>
2026-06-11 12:38:21 +0200

    Update mapPage.jsx

--- frontend/src/pages/mapPage.jsx
+++ frontend/src/pages/mapPage.jsx
@@ -41,11 +41,11 @@ function MapPage() {
 
         const clampedX = Math.max(
             0,
-            Math.min(newX, mapRect.width)
+            Math.min(newX, mapRect.width - 120)
         );
         const clampedY = Math.max(
             0,
-            Math.min(newY, mapRect.height)
+            Math.min(newY, mapRect.height - 120)
         );
 
         setBoxes((prev) =>
