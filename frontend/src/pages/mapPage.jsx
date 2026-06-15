c890634443385b1fa60a695d0717ff727d634d6e
Mr penington <benjaminbkris@gmail.com>
2026-06-15 09:37:37 +0200

    Update mapPage.jsx

--- frontend/src/pages/mapPage.jsx
+++ frontend/src/pages/mapPage.jsx
@@ -104,7 +104,7 @@ function MapPage() {
             setBoxes(prev => [
                 ...prev,
                 {
-                    id: pins.length,
+                    id: pins.length + 1,
                     x: MAP_WIDTH / 2,
                     y: MAP_HEIGHT / 2,
                     text: text
@@ -157,7 +157,7 @@ function MapPage() {
                                 transform: "translate(-50%, -50%)"
                             }}
                         >
-                            {box.text}
+                            {box.id}
                         </div>
                     ))}
                 </div>
