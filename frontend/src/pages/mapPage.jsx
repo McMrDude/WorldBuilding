d30f3a64f7b4761dc54c18e1732b01487e96f792
Mr penington <benjaminbkris@gmail.com>
2026-06-16 10:56:53 +0200

    e

--- frontend/src/pages/mapPage.jsx
+++ frontend/src/pages/mapPage.jsx
@@ -152,7 +152,7 @@ function MapPage() {
     }
 
     return(
-        <div className='bigAssDiv' style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', }}>
+        <div className='bigAssDiv' style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', minHeight: "100vh", }}>
             <h1>MAP</h1>
 
             <div style={{ display: 'flex' }}>
