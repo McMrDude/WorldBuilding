4e27c356c0ab0e3fcaaf24ec94e479d77cdb3c3a
Mr penington <benjaminbkris@gmail.com>
2026-06-22 14:48:34 +0200

    Update areaBox.jsx

--- frontend/src/components/areaBox.jsx
+++ frontend/src/components/areaBox.jsx
@@ -147,7 +147,7 @@ function AreaBox({ onClose, pinID, }) {
                         />
                     }
 
-                    { chosenCharacters || edit ? 
+                    { chosenCharacters.length > 0 || !edit ? 
                         (
                             <div style={{ display: "flex", flexDirection: "row"}}>
                                 { chosenCharacters.map((character) => (
